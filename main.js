9b147fb4cbae75650de88ce7c6b70a8eb374932e
Jarvis <709934831@qq.com>
2018-01-13 22:48:59 +0800

    睡觉了

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 window.onload = function(){
     const Swiper = document.getElementById("Swiper");//轮播图内容
-    var img = Swiper.firstElementChild;//轮播图
-    var imgWidth = img.offsetWidth;//图片的宽度    
-    var xScroll = function xScroll(){
-    setInterval(next,1500);
-    }();
-
+    const img = Swiper.firstElementChild;//轮播图
+    const imgWidth = img.offsetWidth;//图片的宽度
+    const imgs = Swiper.children.length;
+   // var xScroll = setInterval(pre,500);
+    Swiper.addEventListener("mouseleave",function(){setInterval(pre,500)});
+    Swiper.removeEventListener("mouseenter",function(){setInterval(pre,500)});
     function next() {
-        newLeft = Swiper.style.left!==""?parseInt(Swiper.style.left):parseInt(Swiper.offsetLeft);//判断左边距
+        let newLeft =  offsetleft();
         newLeft -=imgWidth;//新的左边距
-        if(newLeft !== -imgWidth*Swiper.children.length)//是否是最后一个图片
+        if(newLeft !== -imgWidth*imgs)//是否是最后一个图片
         {Swiper.style.left = newLeft + 'px';}//每次左移一个图片的宽度
         else{Swiper.style.left = "0px";}//返回到第一个
-    }
+    };
     function pre(){
-        newLeft = Swiper.style.left!==""?parseInt(Swiper.style.left):parseInt(Swiper.offsetLeft);//判断左边距
-        newLeft +=imgWidth;//新的左边距
-        if(newLeft === -imgWidth)//是否是第一张图片
-        {Swiper.style.left = -imgWidth*Swiper.children.length +"px";}//返回到最后一个
-         // else{Swiper.style.left = newLeft + 'px';}//每次右移一个图片的宽度
-    }           
+        newLeft = offsetleft();
+        newLeft += imgWidth;//新的左边距
+        if(newLeft === 0)//是否是第一张图片
+        {Swiper.style.left = -imgWidth*(imgs-1) + "px"}//返回到最后一个
+        else{Swiper.style.left = newLeft + 'px';}//每次右移一个图片的宽度
+    };
+    function offsetleft(){//判断左边距
+        return Swiper.style.left!==""?parseInt(Swiper.style.left):parseInt(Swiper.offsetLeft);
+    }
 
 }
