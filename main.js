5f840584edf559d5519a37c96c989e750c731d15
Jarvis <709934831@qq.com>
2018-01-13 21:56:38 +0800

    pre

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = function(){
     const img = Swiper.firstElementChild;//轮播图
     const imgWidth = img.offsetWidth;//图片的宽度
     var xScroll = function xScroll(){
-        setInterval(next,1500);
+    setInterval(pre,1500);
     }();
 
     function next() {
@@ -12,6 +12,13 @@ window.onload = function(){
         if(newLeft !== -imgWidth*Swiper.children.length)//是否是最后一个图片
         {Swiper.style.left = newLeft + 'px';}//每次左移一个图片的宽度
         else{Swiper.style.left = "0px";}//返回到第一个
+    }
+    function pre(){
+        newLeft = Swiper.style.left!==""?parseInt(Swiper.style.left):parseInt(Swiper.offsetLeft);//判断左边距
+        newLeft +=imgWidth;//新的左边距
+        if(newLeft === -imgWidth)//是否是第一张图片
+        {Swiper.style.left = -imgWidth*Swiper.children.length +"px";}//返回到最后一个
+        else{Swiper.style.left = newLeft + 'px';}//每次右移一个图片的宽度
     }           
 
 }
