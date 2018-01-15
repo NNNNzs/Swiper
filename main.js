56632be2c2b9deed27b42f342fe998c9d07a98ea
NNNNzs <709934831@qq.com>
2018-01-15 08:01:15 +0800

    2018-1-15

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ window.onload = function(){
     const img = Swiper.firstElementChild;//轮播图
     const imgWidth = img.offsetWidth;//图片的宽度
     const imgs = Swiper.children.length;
-   // var xScroll = setInterval(pre,500);
-    Swiper.addEventListener("mouseleave",function(){setInterval(pre,500)});
-    Swiper.removeEventListener("mouseenter",function(){setInterval(pre,500)});
+    var xScroll = setInterval(function(){pre();console.log(whichIndex());},1300);
+
     function next() {
         let newLeft =  offsetleft();
         newLeft -=imgWidth;//新的左边距
@@ -16,12 +15,16 @@ window.onload = function(){
     function pre(){
         newLeft = offsetleft();
         newLeft += imgWidth;//新的左边距
-        if(newLeft === 0)//是否是第一张图片
+        if(newLeft === imgWidth )//是否是第一张图片
         {Swiper.style.left = -imgWidth*(imgs-1) + "px"}//返回到最后一个
         else{Swiper.style.left = newLeft + 'px';}//每次右移一个图片的宽度
     };
     function offsetleft(){//判断左边距
         return Swiper.style.left!==""?parseInt(Swiper.style.left):parseInt(Swiper.offsetLeft);
     }
-
+    
+    function whichIndex(){//判断当前是第几张图片
+        return ~~-offsetleft()/imgWidth;
+    }
+    //yes
 }
